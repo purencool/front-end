e758562971bfebc2c22445ec6a719422f136579b
purencool <purencool@gmail.com>
2018-02-14 17:23:34 +1100

    Created grid layout

--- css-grid.php
+++ css-grid.php
@@ -3,12 +3,62 @@
     <head>
     <title>CSS grid</title>
     <?php include './parts/head.php'; ?>
-        <link rel="stylesheet" href="css/grig.css">
+        <link rel="stylesheet" href="css/grid.css">
     </head>
     <body>
         <?php include './parts/navigation.php'; ?>
         <main>
             <!-- Add css example here -->
+            <div id="content">
+               <section id="description">
+                  <h1>Html5  H1 section</h1>
+                  <p>This is the text of a h1 section</p>
+                </section>
+
+                 <article class="article">
+                   <header>
+                      <h1>Displaying a CSS Grid example</h1>
+                   </header>
+                   <p>
+                     Displaying a CSS Grid example paragraph in an article
+                   </p>
+                   <h2>A random h2</h2>
+                   <p>
+                    Tellus in leo ac eget eu diam mollis est. Amet pulvinar
+                    ullamcorper laoreet penatibus vel scelerisque quia dui
+                    et vestibulum ipsum tristique orci enim phasellus congue laoreet.
+                    Ipsum eget lorem fringilla risus enim. Et tincidunt neque. Sed semper per.
+                    Consectetuer sunt pellentesque. Consectetuer vehicula justo.
+                    Vivamus aliquam luctus justo dolor sed.
+                    Venenatis suscipit eu sit neque lorem. Tincidunt vitae commodo.
+                   </p>
+                </article>
+                <aside class="aside">
+                  <h3>This is the aside txt</h3>
+                   <p>This is the asides paragraph and some extra text to see how a the CSS grid likes a lot of text</p>
+                </aside>
+              </div>
+              <section id="three-cols">
+                <h4>Three columns</h4>
+                <div>1</div>
+                <div>2</div>
+                <div>3</div>
+             </section>
+             <section id="four-cols">
+                <h5>Four columns</h5>
+                <div>1</div>
+                <div>2</div>
+                <div>3</div>
+                <div>4</div>
+             </section>
+             <section id="five-cols">
+                <h6>Five columns</h6>
+                <div>1</div>
+                <div>2</div>
+                <div>3</div>
+                <div>4</div>
+                <div>5</div>
+             </section>
             <!-- End css examples -->
         </main>
         <?php include './parts/footer.php'; ?>
